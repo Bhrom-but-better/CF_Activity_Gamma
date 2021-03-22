5f0683e643370b8913da42bcaad136f026fcc7b6
AlifArshad <alif.arshad.bakshi@gmail.com>
2021-03-22 20:08:28 +0600

    files linked

--- getUsernames.py
+++ getUsernames.py
@@ -2,20 +2,29 @@ import bs4
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
 
-my_url = 'https://codeforces.com/ratings/organization/865'
-# headers={'User-Agent': 'Mozilla/5.0'} for avpiding http error
-req = Request(my_url, headers={'User-Agent': 'Mozilla/5.0'})
-page_html = urlopen(req).read()
-page_soup = soup(page_html, "lxml")
-table = page_soup.findAll('table', {'class': ''})[3]
-rows = table.findAll('tr')
 
+def get_usernames(organization_id):
 
-# parsing
-for row in rows:
-    csvRow = []
-    for cell in row.findAll(['td']):
-        csvRow.append(cell.get_text().strip())
-        # print(cell)
-    if csvRow:
-        print(csvRow[1])
+    users = []
+
+    my_url = 'https://codeforces.com/ratings/organization/865'
+    # headers={'User-Agent': 'Mozilla/5.0'} for avpiding http error
+    req = Request(my_url, headers={'User-Agent': 'Mozilla/5.0'})
+    page_html = urlopen(req).read()
+    page_soup = soup(page_html, "lxml")
+    table = page_soup.findAll('table', {'class': ''})[3]
+    rows = table.findAll('tr')
+
+    # parsing
+    for row in rows:
+        line = []
+        for cell in row.findAll(['td']):
+            line.append(cell.get_text().strip())
+            # print(cell)
+        if line:
+            #print(line[1])
+            users.append(line[1])
+
+    return users
+
+# get_usernames(846)
--- main.py
+++ main.py
@@ -2,32 +2,49 @@ import json
 import requests
 import datetime
 import sys
+import pprint
+import getUsernames
 
-username = input("Handle: ")
-data = requests.get("https://codeforces.com/api/user.status?handle=" + username + "&from=1&count=500").json()
 
-print(username)
+# newList = getUsernames.get_usernames(452)
 
-problems_count = {}
+# pprint.pprint(newList)
 
-for submission in data["result"]:
-    try:
-        time = datetime.datetime.fromtimestamp(submission["creationTimeSeconds"])
-        day = time.day
-        month = time.month
-        year = time.year
-        print(submission["contestId"], submission["problem"]["index"], submission["problem"]["name"],
-              submission["verdict"], day, time.strftime("%B"), year)
-        problem = str(submission["contestId"]) + str(submission["problem"]["index"])
-        if problem not in problems_count.keys():
-            problems_count[problem] = 1
-        else:
-            problems_count[problem] += 1
-    except:
-        print("Not found")
+if __name__ == "__main__":
+    
+    newList = getUsernames.get_usernames(452)
 
-try:
-    max_submissions = max(problems_count, key=problems_count.get)
-    print("Maximum submission: " + max_submissions, problems_count[max_submissions])
-except:
-    print("Not found")
+    for username in newList:
+
+        data = requests.get("https://codeforces.com/api/user.status?handle=" +
+                            username + "&from=1&count=500").json()
+
+
+        problems_count = {}
+
+        try:
+            for submission in data["result"]:
+                time = datetime.datetime.fromtimestamp(
+                    submission["creationTimeSeconds"])
+                day = time.day
+                month = time.month
+                year = time.year
+                if "contestId" in submission:
+                    # print(submission["contestId"], submission["problem"]["index"], submission["problem"]["name"],
+                    #       submission["verdict"], day, time.strftime("%B"), year)
+                    problem = str(submission["contestId"]) + \
+                        str(submission["problem"]["index"])
+                    if problem not in problems_count.keys():
+                        problems_count[problem] = 1
+                    else:
+                        problems_count[problem] += 1
+        except ():
+            print("Not found")
+            quit()
+
+        try:
+            max_submissions = max(problems_count, key=problems_count.get)
+            print(username + ", Maximum submission: " + max_submissions,
+                problems_count[max_submissions])
+        except ():
+            print("Not found")
